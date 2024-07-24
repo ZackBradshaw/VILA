804cfe32a807f7a88efc7653b22088249d09e82e
zack <zackzbradshaw@gmail.com>
2024-07-24 18:28:38 -0500

    feat: update finetune sh

--- scripts/v1_5/release/8b/3_sft.sh
+++ scripts/v1_5/release/8b/3_sft.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
+# Fetch master address and node list
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=${master_addr:-"$IP_ADDR"}
+export MASTER_ADDR=${master_addr:-"199.204.135.78"}
 export CURRENT_RANK=${SLURM_PROCID:-"0"}
 worker_list=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | tr '\n' ' ')
 n_node=${SLURM_JOB_NUM_NODES:-1}
 
-echo "MASTER_ADDR="$MASTER_ADDR
+# Check if necessary variables are set
+if [ -z "$MASTER_ADDR" ] || [ -z "$CURRENT_RANK" ] || [ -z "$n_node" ]; then        
+    echo "Error: One or more environment variables are not set correctly."
+    exit 1
+fi
+
+echo "MASTER_ADDR=$MASTER_ADDR"
 echo "JobID: $SLURM_JOB_ID | Full list: $worker_list"
 
-# OUTPUT of stage 1 script
-STAGE1_PATH=$1
-# for example, llava-v1.5-7b-mm-align
+n_nodes=1
+bs=16
+STAGE2_PATH=$1
 OUTPUT=$2
 
-export NCCL_IB_SL=1
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-#export NCCL_DEBUG=INFO
-export NCCL_ASYNC_ERROR_HANDLING=1
-#export CUDA_LAUNCH_BLOCKING=1
-
-
-
-n_node=$SLURM_JOB_NUM_NODES
-bs=$((128 / n_node))
-echo "number of nodes:" $n_node
-echo "per device batch size:" $bs
-echo "node rank:" $SLURM_PROCID
-
-torchrun --nnodes=$n_node --nproc_per_node=8 --master_port=25001 \
+# Run torchrun with additional checks
+torchrun --nnodes=$n_node --nproc_per_node=4 --master_port=25001 \
     --master_addr $MASTER_ADDR --node_rank=$CURRENT_RANK \
     llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path $BASE_MODEL_PATH \
+    --model_name_or_path $STAGE2_PATH \
     --version llama_3 \
-    --data_mixture coyo_25m+mmc4core+sharegpt4v_pretrain \
+    --data_path bluepy \
     --vision_tower google/siglip-so400m-patch14-384 \
     --mm_vision_select_feature cls_patch \
     --mm_projector mlp_downsample \
-    --tune_vision_tower False \
+    --tune_vision_tower True \
     --tune_mm_projector True \
     --tune_language_model True \
     --mm_vision_select_layer -2 \
@@ -46,16 +40,16 @@ torchrun --nnodes=$n_node --nproc_per_node=8 --master_port=25001 \
     --mm_use_im_patch_token False \
     --image_aspect_ratio resize \
     --bf16 True \
-    --output_dir ./checkpoints/$OUTPUT \
+    --output_dir ./finetune/ \
     --num_train_epochs 1 \
     --per_device_train_batch_size $bs \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 200 \
+    --save_steps 100 \
     --save_total_limit 1 \
-    --learning_rate 5e-5 \
+    --learning_rate 1e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -63,6 +57,7 @@ torchrun --nnodes=$n_node --nproc_per_node=8 --master_port=25001 \
     --tf32 True \
     --model_max_length 4096 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 16 \
     --lazy_preprocess True \
+    --vflan_no_system_prompt True \
     --report_to wandb
